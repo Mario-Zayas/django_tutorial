7c3fc5a5c275f058b35fa8ff620038bb0c181848
Mario-Zayas <mzaygar034@gmail.com>
2024-03-07 10:27:18 +0100

    Cambios

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,4 @@ sleep 8
 python3 manage.py migrate
 python3 manage.py createsuperuser --noinput
 python3 manage.py collectstatic --no-input
-python3 manage.py runserver 0.0.0.0:8003
+python3 manage.py runserver 0.0.0.0:8004
